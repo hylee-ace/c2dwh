257577e7625778be45fd49276a15fe2297df69d6
hylee-ace <quangduy.huynh.arc97@gmail.com>
2025-08-23 23:43:47 +0700

    Minor fixes

--- scripts/webcrawler/models.py
+++ scripts/webcrawler/models.py
@@ -9,6 +9,10 @@ class Product:
     price: int | float = None
     onsale_price: int | float = None
     brand: str = None
+    category: str = None
+    os: str = None
+    cpu: str = None
+    gpu: str = None
     stock: int = None
     rating: float = None
     reviews_count: int = None
@@ -19,12 +23,6 @@ class Product:
     created_at: datetime = None
     updated_at: datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-
-@dataclass
-class Phone(Product):
-    category: str = "Smartphone"
-    os: str = None
-
     def info(self):
         if not self.price and not self.stock:
             self.is_selling = False
@@ -35,42 +33,13 @@ class Phone(Product):
             "price": self.price,
             "onsale_price": self.onsale_price,
             "brand": self.brand,
-            "os": self.os,
-            "stock": self.stock,
-            "rating": self.rating,
-            "reviews_count": self.reviews_count,
             "category": self.category,
-            "is_new": self.is_new,
-            "is_selling": self.is_selling,
-            "url": self.url,
-            "retailer": self.retailer,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
-        }
-
-
-@dataclass
-class Laptop(Product):
-    category: str = "Laptop"
-    cpu: str = None
-    gpu: str = None
-
-    def info(self):
-        if not self.price and not self.stock:
-            self.is_selling = False
-
-        return {
-            "id": self.product_id,
-            "name": self.name,
-            "price": self.price,
-            "onsale_price": self.onsale_price,
-            "brand": self.brand,
+            "os": self.os,
             "cpu": self.cpu,
             "gpu": self.gpu,
             "stock": self.stock,
             "rating": self.rating,
             "reviews_count": self.reviews_count,
-            "category": self.category,
             "is_new": self.is_new,
             "is_selling": self.is_selling,
             "url": self.url,
